4140a9b2e7183637b074c36cfe06c7be37cc50c8
fifiswang <dragstarfg@gmail.com>
2025-04-13 19:39:36 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+// Projekt BeFit – Autor: Filip Zabłocki
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace BeFit
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Projekt BeFit
+
+Autor: Filip Zabłocki
+
+Kompletna aplikacja ASP.NET Core MVC z systemem logowania, operacjami CRUD i statystykami użytkownika.
